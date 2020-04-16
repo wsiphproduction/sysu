de04b97d2bb726e5f1e2f7cbcaffe01119ed6881
darrenbawag <renash12@gmail.com>
2020-04-16 10:45:07 +0800

    SYSU Updated Add to Cart

--- js/script.js
+++ js/script.js
@@ -178,3 +178,57 @@ $("input[type='number']").keydown(function() {
     ? false
     : true;
 });
+
+
+// Quantity
+
+function customQuantity() {
+  /** Custom Input number increment js **/
+  jQuery(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>'
+  ).insertAfter(".quantity input");
+  jQuery(".quantity").each(function() {
+    var spinner = jQuery(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find(".quantity-up"),
+      btnDown = spinner.find(".quantity-down"),
+      min = input.attr("min"),
+      max = input.attr("max"),
+      valOfAmout = input.val(),
+      newVal = 0;
+
+    btnUp.on("click", function() {
+      var varholder = input.val();
+      var oldValue = parseFloat(input.val());
+
+      if (varholder === "") {
+        var newVal = 1;
+      } else {
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+    btnDown.on("click", function() {
+      var varholder = input.val();
+      var oldValue = parseFloat(input.val());
+
+      if (varholder === "") {
+        var newVal = 1;
+      } else {
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
+}
+customQuantity();
